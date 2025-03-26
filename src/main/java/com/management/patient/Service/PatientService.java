9842a18481e258c53329a68a96cdf21b70939752
AryanSoni27 <aryansoni33635@gmail.com>
2025-03-26 18:20:26 +0530

    Created service for patient

--- src/main/java/com/management/patient/Service/PatientService.java
+++ src/main/java/com/management/patient/Service/PatientService.java
@@ -1,4 +1,22 @@
 package com.management.patient.Service;
 
+import com.management.patient.Dto.PatientResponseDTO;
+import com.management.patient.Entity.Patient;
+import com.management.patient.Mapper.PatientMapper;
+import com.management.patient.Repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PatientService {
+    @Autowired
+    private PatientRepository patientRepository;
+
+    public List<PatientResponseDTO> getPatients(){
+        List<Patient> patients = patientRepository.findAll();
+        return patients.stream()
+                .map(PatientMapper::toDTO).toList();
+    }
 }
