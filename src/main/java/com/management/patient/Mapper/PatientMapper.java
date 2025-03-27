4ba1138aaa0bdb349cdb7cb8cd239032fb240470
AryanSoni27 <aryansoni33635@gmail.com>
2025-03-27 17:52:12 +0530

    Created mapper for patient request

--- src/main/java/com/management/patient/Mapper/PatientMapper.java
+++ src/main/java/com/management/patient/Mapper/PatientMapper.java
@@ -1,5 +1,6 @@
 package com.management.patient.Mapper;
 
+import com.management.patient.Dto.PatientRequestDTO;
 import com.management.patient.Dto.PatientResponseDTO;
 import com.management.patient.Entity.Patient;
 
@@ -12,5 +13,15 @@ public class PatientMapper {
         patientResponseDTO.setAddress(patient.getAddress());
         patientResponseDTO.setDateOfBirth(patient.getDateOfBirth().toString());
         return patientResponseDTO;
+    }
+
+    public static PatientRequestDTO toModel(Patient patient){
+        PatientRequestDTO patientRequestDTO = new PatientRequestDTO();
+        patientRequestDTO.setName(patient.getName());
+        patientRequestDTO.setEmail(patient.getEmail());
+        patientRequestDTO.setAddress(patient.getAddress());
+        patientRequestDTO.setDateOfBirth(patient.getDateOfBirth().toString());
+        patientRequestDTO.setRegisteredDate(patient.getRegisteredDate().toString());
+        return patientRequestDTO;
     }
 }
