84c3ea8b528cb254af96236873b4180226420fe8
AryanSoni27 <aryansoni33635@gmail.com>
2025-03-28 11:42:25 +0530

    Created controller for creating new patient

--- src/main/java/com/management/patient/Controller/PatientController.java
+++ src/main/java/com/management/patient/Controller/PatientController.java
@@ -1,12 +1,12 @@
 package com.management.patient.Controller;
 
+import com.management.patient.Dto.PatientRequestDTO;
 import com.management.patient.Dto.PatientResponseDTO;
 import com.management.patient.Service.PatientService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,8 +17,14 @@ public class PatientController {
     private PatientService patientService;
 
     @GetMapping
-    public ResponseEntity<List<PatientResponseDTO>> getPatients(){
+    public ResponseEntity<List<PatientResponseDTO>> getPatients() {
         List<PatientResponseDTO> patients = patientService.getPatients();
         return ResponseEntity.ok().body(patients);
+    }
+
+    @PostMapping
+    public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO) {
+        PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
+        return ResponseEntity.ok().body(patientResponseDTO);
     }
 }
