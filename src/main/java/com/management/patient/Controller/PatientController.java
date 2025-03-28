3767b2021b3a8bb204eddd4ae8038f80f934128b
AryanSoni27 <aryansoni33635@gmail.com>
2025-03-28 16:13:04 +0530

    Implemented open api for generation of docs

--- src/main/java/com/management/patient/Controller/PatientController.java
+++ src/main/java/com/management/patient/Controller/PatientController.java
@@ -4,6 +4,8 @@ import com.management.patient.Dto.PatientRequestDTO;
 import com.management.patient.Dto.PatientResponseDTO;
 import com.management.patient.Dto.Validators.CreatePatientValidationGroup;
 import com.management.patient.Service.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.groups.Default;
 import lombok.Builder;
@@ -17,18 +19,20 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/patients")
+@Tag(name = "Patient", description = "API for managing patients")
 public class PatientController {
     @Autowired
     private PatientService patientService;
 
     @GetMapping
+    @Operation(summary = "Get all patients")
     public ResponseEntity<List<PatientResponseDTO>> getPatients() {
         List<PatientResponseDTO> patients = patientService.getPatients();
         return ResponseEntity.ok().body(patients);
     }
 
     @PostMapping
-//    @Operation(summary = "Create a new Patient")
+    @Operation(summary = "Create a new Patient")
     public ResponseEntity<PatientResponseDTO> createPatient(
             @Validated({Default.class, CreatePatientValidationGroup.class})
             @RequestBody PatientRequestDTO patientRequestDTO) throws Exception {
@@ -40,6 +44,7 @@ public class PatientController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update an existing patient")
     public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable UUID id,
                                                             @Validated({Default.class}) @RequestBody PatientRequestDTO patientRequestDTO){
         PatientResponseDTO patientResponseDTO = patientService.updatePatient(id,
@@ -48,6 +53,7 @@ public class PatientController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete an patient")
     public ResponseEntity<?> deletePatient(@PathVariable UUID id){
         patientService.deletePatient(id);
         return ResponseEntity.ok().body("Deleted Patient successfully");
